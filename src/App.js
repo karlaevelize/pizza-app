c193c4ea4ea5ec71b8ad55f26222acfbce2fe3ba
karlaevelize <k.evelize@gmail.com>
2022-08-18 14:59:32 +0200

    persisting the state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./App.css";
+import { useEffect } from "react"
+import { useDispatch } from "react-redux"  
 import { Routes, Route } from "react-router-dom"
 import PizzaList from "./pages/PizzaList";
 import PostList from "./pages/PostList";
 import Login from "./pages/Login"
 import Navbar from "./components/Navbar";
+import { persistLogin } from "./store/user/thunks"
 
 function App() {
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(persistLogin())
+  }, [])
+
   return (
     <div className="App">
       <Navbar/>
--- src/store/user/thunks.js
+++ src/store/user/thunks.js
@@ -6,16 +6,37 @@ const API_URL = "https://codaisseur-coders-network.herokuapp.com"
 export const login = (email, password) => async (dispatch, getState) => {
   const response = await axios.post(`${API_URL}/login`, {email, password})
   // console.log("response", response)
-
   const token = response.data.jwt
-  
+  localStorage.setItem("token", token)
+
+  const profileResponse = await axios.get(`${API_URL}/me`, {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  })
+  // console.log("profile", profileResponse)
+  const profile = profileResponse.data
+
+  dispatch(userLogin({ token, profile }))
+}
+
+//1. Put the token in local storage
+//2. Write a function to check if token is in local storage
+//3. If so, make a request to get the profile
+//4. Function should run whenever we refresh the page
+
+export const persistLogin = () => async (dispatch, getState) => {
+  const token = localStorage.getItem("token")
+
+  if (!token) return
+
   const profileResponse = await axios.get(`${API_URL}/me`, {
     headers: {
       Authorization: `Bearer ${token}`
     }
   })
 
-  // console.log("profile", profileResponse)
   const profile = profileResponse.data
+
   dispatch(userLogin({ token, profile }))
 }
